d99fc9d1f5150a1c9a7dca2533e8938ac5e2549f
Diego Lima da Costa <dicommunitas@gmail.com>
2021-11-21 22:30:38 -0300

    Subindo projeto

---
+++ myApp1/target/generated-sources/annotations/com/myapp/domain/A_.java
@@ -0,0 +1,19 @@
+package com.myapp.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(A.class)
+public abstract class A_ {
+
+	public static volatile SetAttribute<A, B> bs;
+	public static volatile SingularAttribute<A, Long> id;
+
+	public static final String BS = "bs";
+	public static final String ID = "id";
+
+}
+
---
+++ myApp2/target/generated-sources/annotations/com/myapp/domain/C_.java
@@ -0,0 +1,16 @@
+package com.myapp.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(C.class)
+public abstract class C_ {
+
+	public static volatile SingularAttribute<C, Long> id;
+
+	public static final String ID = "id";
+
+}
+
