2c472a9609d1c17db6523fdbadb44936ce742423
Dicommunitas <dicommunitas@gmail.com>
2022-04-14 07:22:23 +0000

    Novos testes, problema com login continua

--- teste/src/main/java/com/operacional/controleoperacional/web/rest/vm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * View Models used by Spring MVC REST controllers.
- */
-package com.operacional.controleoperacional.web.rest.vm;
--- teste/src/test/java/com/operacional/controleoperacional/cucumber/CucumberIT.java
+++
@@ -1,8 +0,0 @@
-package com.operacional.controleoperacional.cucumber;
-
-import com.operacional.controleoperacional.IntegrationTest;
-import io.cucumber.junit.platform.engine.Cucumber;
-
-@Cucumber
-@IntegrationTest
-class CucumberIT {}
